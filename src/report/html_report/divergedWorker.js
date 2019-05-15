76573954c1f8a529a81b6dd7d19088644621320b
alexandrachupryna <alexandrachuprina93@gmail.com>
2019-05-15 17:49:02 +0300

    removed report after deploy

--- src/report/html_report/divergedWorker.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable strict */
-importScripts('diff.js');
-importScripts('diverged.js');
-self.addEventListener('message', function(e) {
-  self.postMessage(diverged(...e.data.divergedInput));
-  self.close();
-}, false);
