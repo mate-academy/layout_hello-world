bcd8ec8c2c39c3114dd6d5508a74a7365fe1b6e7
gestap <gestap9@gmail.com>
2019-07-24 18:04:42 +0300

    added module backstopConfig.

--- config/backstop/backstopConfig.js
+++ config/backstop/backstopConfig.js
@@ -1,26 +1,20 @@
 'use strict';
 
 // https://github.com/garris/BackstopJS#advanced-scenarios
-const basicScenario = {
-  label: 'test', // name of the test
-  url: 'http://localhost:8080/index.html',
-  referenceUrl: 'https://mate-academy.github.io/layout_solutions/moyo-header/',
-  readyEvent: '',
-  readySelector: '',
-  delay: 1000,
-  hideSelectors: ['[data-qa-hidden]'],
-  removeSelectors: ['[data-qa-remove]'],
-  hoverSelector: '',
-  clickSelector: '',
-  postInteractionWait: 0,
-  selectors: '',
-  selectorExpansion: true,
-  expect: 0,
-  misMatchThreshold: 1, // 1% of 100%
+
+const backstop = require('@mate-academy/backstop-config');
+
+const { basicScenario } = backstop;
+
+const basic = {
+  ...basicScenario,
+  label: 'Elementary test',
+  referenceUrl:
+    'https://mate-academy.github.io/layout_solutions/moyo-header/',
 };
 
-module.exports = {
-  id: 'test',
+const config = {
+  ...backstop,
   onBeforeScript: 'puppet/onBefore.js',
   onReadyScript: 'puppet/onReady.js',
   viewports: [
@@ -37,43 +31,28 @@ module.exports = {
   ],
   scenarios: [
     {
-      ...basicScenario,
+      ...basic,
       label: 'header-tag',
       selectors: ['header'],
     },
     {
-      ...basicScenario,
+      ...basic,
       label: 'nav-tag',
       selectors: ['nav'],
     },
     {
-      ...basicScenario,
+      ...basic,
       label: 'nav-item-hovered',
       selectors: ['[data-qa="hover"]'],
       hoverSelector: '[data-qa="hover"]',
       postInteractionWait: 1000,
     },
     {
-      ...basicScenario,
+      ...basic,
       label: 'nav-item-active',
       selectors: ['a.is-active'],
     },
-    // define here scenarios for testing
   ],
-  paths: {
-    bitmaps_reference: 'backstop_data/bitmaps_reference',
-    bitmaps_test: 'backstop_data/bitmaps_test',
-    engine_scripts: 'backstop_data/engine_scripts',
-    html_report: 'backstop_data/html_report',
-    ci_report: 'backstop_data/ci_report',
-  },
-  report: ['browser'],
-  engine: 'puppeteer',
-  engineOptions: {
-    args: ['--no-sandbox'],
-  },
-  asyncCaptureLimit: 5,
-  asyncCompareLimit: 50,
-  debug: false,
-  debugWindow: false,
 };
+
+module.exports = config;
