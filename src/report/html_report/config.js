501bcc7abdc93d259e3db81f0dc7e1c794c9eb38
Shklyar Igor <shklyarigor2015@gmail.com>
2019-07-25 16:51:33 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,29 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1344142670_Elementary_test_0_document_0_custom-size.png",
+        "test": "..\\bitmaps_test\\20190725-161758\\1344142670_Elementary_test_0_document_0_custom-size.png",
+        "selector": "document",
+        "fileName": "1344142670_Elementary_test_0_document_0_custom-size.png",
+        "label": "Elementary test",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
+        "expect": 0,
+        "viewportLabel": "custom-size",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
