6e9410f125dd46fe019a2a9ecf9f8ab015392cd4
VadimK <djvadya16@gmail.com>
2019-08-27 13:06:27 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,31 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\683167608_Elementary_test_0_document_0_custom-size.png",
+        "test": "..\\bitmaps_test\\20190827-125712\\683167608_Elementary_test_0_document_0_custom-size.png",
+        "selector": "document",
+        "fileName": "683167608_Elementary_test_0_document_0_custom-size.png",
+        "label": "Elementary test",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
+        "expect": 0,
+        "viewportLabel": "custom-size",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "3.51",
+          "analysisTime": 12
+        },
+        "diffImage": "..\\bitmaps_test\\20190827-125712\\failed_diff_683167608_Elementary_test_0_document_0_custom-size.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
