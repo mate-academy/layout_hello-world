72bac379e15cb7f8e2f9dcfac68b2da92eab579d
mgrinko <glukym@gmail.com>
2019-11-05 14:28:05 +0200

    Initial commit

---
+++ config/backstop/backstopConfig.js
@@ -0,0 +1,62 @@
+'use strict';
+// https://github.com/garris/BackstopJS#advanced-scenarios
+
+const backstop = require('@mate-academy/backstop-config');
+const { basicScenario } = backstop;
+
+const basic = {
+  ...basicScenario,
+  label: 'Elementary test',
+  referenceUrl: basicScenario.referenceUrl + '/stars/',
+};
+
+const config = {
+  ...backstop,
+  fileNameTemplate: '{scenarioLabel}',
+  viewports: [
+    {
+      name: 'tablet_h',
+      width: 1024,
+      height: 768,
+    },
+  ],
+  scenarios: [
+    {
+      ...basic,
+      label: 'Entire document',
+      selectors: ['document'],
+    },
+    {
+      ...basic,
+      label: 'stars--0',
+      selectors: ['.stars--0'],
+    },
+    {
+      ...basic,
+      label: 'stars--1',
+      selectors: ['.stars--1'],
+    },
+    {
+      ...basic,
+      label: 'stars--2',
+      selectors: ['.stars--2'],
+    },
+    {
+      ...basic,
+      label: 'stars--3',
+      selectors: ['.stars--3'],
+    },
+    {
+      ...basic,
+      label: 'stars--4',
+      selectors: ['.stars--4'],
+    },
+    {
+      ...basic,
+      label: 'stars--5',
+      selectors: ['.stars--5'],
+    },
+  ],
+};
+
+module.exports = config;
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Product cards
+1. Replace `<your_account>` with your Github username in the links
+    - [DEMO LINK](https://<your_account>.github.io/layout_stars/) <br>
+    - [TEST REPORT LINK](https://<your_account>.github.io/layout_stars/report/html_report/)
+2. Follow [this instructions](https://mate-academy.github.io/layout_task-guideline/)
+
+> Disable `Multiplayer Cursors` in figma to hide other cursors ([Learn how](https://mate-academy.github.io/layout_task-guideline/figma.html#multiplayer-cursors))
+___
+
+## The task
+Implement `stars` block used in [the card](https://www.figma.com/file/bS8N1lTT0Ew0Brf1Nfl36iMV/Product-Cards?node-id=0%3A1).
+- Reset browser's default margins
+- Implement 6 blocks with classes
+  - The first one is general `stars`
+  - Add 5 more blocks with a modifier per block from `stars--1` to `stars--5`
+  - Each block should have a given number of active stars without adding any special class for an individual star. (Use `nth-child`)
+- The star size is fixed
+- The distance between the stars is also fixed
