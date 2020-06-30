5c3d8bca9c0d8b5973639918c9ebc20f16033b71
movchko <lemberg.my.city@gmail.com>
2020-06-30 09:13:36 +0300

    write a short description of the changes you made

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 
-cp -R backstop_data/ src/report
-git add src/
-git commit -m "deploy static content" --no-verify
-git subtree push --prefix src origin gh-pages
-./node_modules/.bin/rimraf src/report
-git add src/
-git commit -m "removed report after deploy" --no-verify
+DEST=$1
+
+# Commit
+echo "git add ${DEST} --force"
+git add ${DEST} --force
+
+echo "git commit -m \"make build\" --no-verify"
+git commit -m "make build" --no-verify
+
+# Push to gh-pages
+echo "git push --delete origin gh-pages"
+git push --delete origin gh-pages
+
+echo "git subtree push --prefix ${DEST} origin gh-pages"
+git subtree push --prefix ${DEST} origin gh-pages
+
+# Clean up
+echo "git reset --soft HEAD^"
+git reset --soft HEAD^
+
+echo "rm -rf ${DEST}"
+rm -rf ${DEST}
+
+echo "git reset -- ${DEST}"
+git reset -- ${DEST}
+
+echo "git checkout ./${DEST}"
+git checkout ./${DEST}
