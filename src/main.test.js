1c5790cf0851da160b920fe925d6351f172c370d
Dima Horshunov <cnimipignak@icloud.com>
2022-11-03 21:50:25 +0200

    setting 2

--- src/main.test.js
+++ src/main.test.js
@@ -162,8 +162,7 @@ describe('Environmental Check', () => {
     if (OS === 'Workflow') {
       const testLinkBody = getSiteBody('[TEST REPORT LINK]', '_report/');
 
-      expect(testLinkBody)
-        .toContain('BackstopJS Report');
+      expect(testLinkBody).toContain('BackstopJS Report');
     }
 
     expect(true)
