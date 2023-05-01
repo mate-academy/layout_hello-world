0db44f2059705ede10a6fabeccb9a1a7a08b8334
Edward <mugivara0309@gmail.com>
2023-05-01 20:55:14 +0200

    make build

---
+++ dist/report/html_report/config.js
@@ -0,0 +1,31 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20230501-203136\\Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
+        "expect": 0,
+        "viewportLabel": "custom-size",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -30
+          },
+          "misMatchPercentage": "12.39",
+          "analysisTime": 11
+        },
+        "diffImage": "..\\bitmaps_test\\20230501-203136\\failed_diff_Entire_document.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
