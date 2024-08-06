0fbd7ceb931f36c49950fa182aea117c363eed8a
Alexandra Hockett <87495152+AlexandraHockett@users.noreply.github.com>
2024-08-06 19:22:07 +0100

    make build

--- dist/report/html_report/config.js
+++ dist/report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20240806-185119\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20240806-191939\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
