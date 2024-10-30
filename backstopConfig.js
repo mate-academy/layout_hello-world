46f6c39c5ecefae6aeab7245c53219006eae8ca9
tecnicorikardo <tecnicorikardo@gmail.com>
2024-10-30 11:44:04 -0300

    Altarado backstopconfig.js

--- backstopConfig.js
+++ backstopConfig.js
@@ -1,25 +1,30 @@
 'use strict';
 
-// https://github.com/garris/BackstopJS#advanced-scenarios
-
+// Importa a configuração básica do BackstopJS
 const backstop = require('@mate-academy/backstop-config');
-
 const { basicScenario } = backstop;
 
+// Configuração do BackstopJS
 const config = {
   ...backstop,
   fileNameTemplate: '{scenarioLabel}',
-  viewports: [{
-    name: 'custom-size',
-    width: 200,
-    height: 50,
-  }],
+  viewports: [
+    {
+      name: 'custom-size',
+      width: 1280, // Ajuste para uma largura padrão de desktop
+      height: 800,  // Ajuste para uma altura padrão de desktop
+    },
+  ],
   scenarios: [
     {
       ...basicScenario,
       label: 'Entire document',
-      referenceUrl: basicScenario.referenceUrl + '/hello-world/',
-    }],
+      referenceUrl: 'https://tecnicorikardo.github.io/layout_hello-world', // URL do seu site publicado
+      url: 'https://tecnicorikardo.github.io/layout_hello-world', // URL para o cenário de teste
+      selectors: ['document'], // Se você quiser capturar o documento inteiro
+    },
+  ],
 };
 
+// Exporta a configuração
 module.exports = config;
