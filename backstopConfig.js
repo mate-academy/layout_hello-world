9f202336ca68d92d0a07665f5f7dd06637178991
tecnicorikardo <tecnicorikardo@gmail.com>
2024-10-30 12:33:59 -0300

    Feita alteração js

--- backstopConfig.js
+++ backstopConfig.js
@@ -7,21 +7,23 @@ const { basicScenario } = backstop;
 // Configuração do BackstopJS
 const config = {
   ...backstop,
-  fileNameTemplate: '{scenarioLabel}',
+  fileNameTemplate: '{scenarioLabel}', // Nome do arquivo para os cenários
   viewports: [
     {
       name: 'custom-size',
-      width: 1280, // Ajuste para uma largura padrão de desktop
-      height: 800,  // Ajuste para uma altura padrão de desktop
+      width: 1280, // Largura padrão de desktop
+      height: 800,  // Altura padrão de desktop
     },
   ],
   scenarios: [
     {
       ...basicScenario,
-      label: 'Entire document',
-      referenceUrl: 'https://tecnicorikardo.github.io/layout_hello-world', // URL do seu site publicado
+      label: 'Entire document', // Rótulo do cenário
+      referenceUrl: 'https://tecnicorikardo.github.io/layout_hello-world', // URL do site publicado
       url: 'https://tecnicorikardo.github.io/layout_hello-world', // URL para o cenário de teste
-      selectors: ['document'], // Se você quiser capturar o documento inteiro
+      selectors: ['document'], // Seletores para capturar o documento inteiro
+      readyEvent: 'DOMContentLoaded', // Aguarda o carregamento do DOM
+      delay: 1000, // Espera um segundo após o carregamento
     },
   ],
 };
