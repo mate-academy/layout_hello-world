9272896fe31aec72be5170766f22b621a0de4b93
denisimus <deoniisii@gmail.cim>
2025-02-04 17:45:30 +0200

    make build

--- dist/report/html_report/config.js
+++ dist/report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document.png",
-        "test": "../bitmaps_test/20250204-174143/Entire_document.png",
+        "test": "../bitmaps_test/20250204-174435/Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
