e8f3d8044265270c08d6bfe1bfdaa632625d9068
Tara <tara.ortega@live.com>
2025-02-19 00:21:34 +0000

    add task solution

--- backstopConfig.js
+++ backstopConfig.js
@@ -15,12 +15,44 @@ const config = {
       width: 200,
       height: 5,
     },
+    {
+      name: '1024px',
+      width: 1024,
+      height: 768,
+    },
+    {
+      name: '1200px',
+      width: 1200,
+      height: 768,
+    },
   ],
   scenarios: [
     {
       ...basicScenario,
       label: 'Entire document',
       referenceUrl: basicScenario.referenceUrl + '/hello-world/',
+    },
+    {
+      ...basic,
+      label: 'Header tag',
+      selectors: ['header'],
+    },
+    {
+      ...basic,
+      label: 'Nav tag',
+      selectors: ['nav'],
+    },
+    {
+      ...basic,
+      label: 'Link with data-qa_hover',
+      selectors: ['[data-qa="hover"]'],
+      hoverSelector: '[data-qa="hover"]',
+      postInteractionWait: 1000,
+    },
+    {
+      ...basic,
+      label: 'Link with class_is-active',
+      selectors: ['a.is-active'],
     },
   ],
 };
