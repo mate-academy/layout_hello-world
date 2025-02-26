cce0bba6c08407bd00300bd8de65b0d69b445fa4
Peterson Paris <140709138+parispeterson89-ops@users.noreply.github.com>
2025-02-26 02:59:55 -0300

    .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config',
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"]
+    }
 };
