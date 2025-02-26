4bf2f4c5f7a74980d7e545ad4b9067d9dd22d464
Peterson Paris <140709138+parispeterson89-ops@users.noreply.github.com>
2025-02-26 03:01:12 -0300

    backstopConfig.js

--- backstopConfig.js
+++ backstopConfig.js
@@ -1,25 +1,45 @@
-'use strict';
-
-// https://github.com/garris/BackstopJS#advanced-scenarios
-
-const backstop = require('@mate-academy/backstop-config');
-
-const { basicScenario } = backstop;
-
-const config = {
-  ...backstop,
-  fileNameTemplate: '{scenarioLabel}',
-  viewports: [{
-    name: 'custom-size',
-    width: 200,
-    height: 50,
-  }],
-  scenarios: [
-    {
-      ...basicScenario,
-      label: 'Entire document',
-      referenceUrl: basicScenario.referenceUrl + '/hello-world/',
-    }],
+module.exports = {
+    "id": "backstop_default",
+    "viewports": [
+        {
+            "label": "phone",
+            "width": 320,
+            "height": 480
+        },
+        {
+            "label": "tablet",
+            "width": 768,
+            "height": 1024
+        },
+        {
+            "label": "desktop",
+            "width": 1280,
+            "height": 800
+        }
+    ],
+    "scenarios": [
+        {
+            "label": "Hello World Page",
+            "url": "http://localhost:3000",
+            "selectors": ["document"],
+            "delay": 500
+        }
+    ],
+    "paths": {
+        "bitmaps_reference": "backstop_data/bitmaps_reference",
+        "bitmaps_test": "backstop_data/bitmaps_test",
+        "engine_scripts": "backstop_data/engine_scripts",
+        "html_report": "backstop_data/html_report",
+        "ci_report": "backstop_data/ci_report"
+    },
+    "report": ["browser"],
+    "engine": "puppeteer",
+    "engineOptions": {
+        "args": ["--no-sandbox"]
+    },
+    "asyncCaptureLimit": 5,
+    "asyncCompareLimit": 50,
+    "debug": false,
+    "debugWindow": false
 };
 
-module.exports = config;
