20db8a07b9a1b6d975d0f12132554692a2938392
TheNash1996 <nash-morris@outlook.com>
2025-05-20 20:23:51 +0100

    Fix: restore proper BackstopJS report in docs/report

--- docs/report/config.js
+++ docs/report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20250520-193147\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20250520-201734\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
