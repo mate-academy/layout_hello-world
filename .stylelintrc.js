356ae9abafe2a1b73687cd48ef0f113baa76c708
IVANIVALDO <142198929+IVANIVALDO@users.noreply.github.com>
2025-07-27 23:52:24 -0300

    Corrige script de teste e configurações do projeto

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-  extends: '@mate-academy/stylelint-config',
-  plugins: ['stylelint-scss'],
-  rules: {},
+  extends: "@mate-academy/stylelint-config",
+  plugins: [
+    "stylelint-scss"
+  ],
+  rules: {}
 };
