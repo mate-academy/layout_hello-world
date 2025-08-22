b4d86a52b6b7721b5799ad7109b93e1c34976a98
Olena <elena.chernyshova01@gmail.com>
2025-08-22 12:13:04 +0300

    fix: add DOCTYPE at the beginning

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+  parser: '@html-eslint/parser',
+  plugins: ['@html-eslint'],
+  extends: ['@mate-academy/eslint-config',],
+  rules: {
+    '@html-eslint/indent': ['error', 2],
+    '@html-eslint/require-lang': 'error',
+  },
+};
+import html from '@html-eslint/eslint-plugin';
+import parser from '@html-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.html'],
+    plugins: {
+      '@html-eslint': html,
+    },
+    languageOptions: {
+      parser: parser,
+    },
+    extends: ['@mate-academy/eslint-config'],
+    rules: {
+      '@html-eslint/indent': ['error', 2],
+      '@html-eslint/require-lang': 'error',
+    },
+  },
+];
