6f5b053d00f37001bee748865c7e13b83e5f0e9f
TiFAu <turchin.anton2@gmail.com>
2025-09-03 05:48:58 +0300

    add changes in  backstopConfig.js

--- backstopConfig.js
+++ backstopConfig.js
@@ -9,17 +9,42 @@ const { basicScenario } = backstop;
 const config = {
   ...backstop,
   fileNameTemplate: '{scenarioLabel}',
-  viewports: [{
-    name: 'custom-size',
-    width: 200,
-    height: 50,
-  }],
+  viewports: [
+    {
+      name: '1024px',
+      width: 1024,
+      height: 768,
+    },
+    {
+      name: '1200px',
+      width: 1200,
+      height: 768,
+    },
+  ],
   scenarios: [
     {
-      ...basicScenario,
-      label: 'Entire document',
-      referenceUrl: basicScenario.referenceUrl + '/hello-world/',
-    }],
+      ...basic,
+      label: 'Header tag',
+      selectors: ['header'],
+    },
+    {
+      ...basic,
+      label: 'Nav tag',
+      selectors: ['nav'],
+    },
+    {
+      ...basic,
+      label: 'Link with data-qa_hover',
+      selectors: ['[data-qa="hover"]'],
+      hoverSelector: '[data-qa="hover"]',
+      postInteractionWait: 1000,
+    },
+    {
+      ...basic,
+      label: 'Link with class_is-active',
+      selectors: ['a.is-active'],
+    },
+  ],
 };
 
 module.exports = config;
