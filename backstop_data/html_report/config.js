7f6d8cbf1b381e7a4726e605ce74bbbc7c929522
luishenriquetp <luishenriquetp@SamsungE35S.luishenriquetp>
2025-11-24 21:40:09 -0300

    Backup commit

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document.png",
-        "test": "../bitmaps_test/20240229-145635/Entire_document.png",
+        "test": "../bitmaps_test/20240305-144630/Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
