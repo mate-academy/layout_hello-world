9980c9f5687ce954a715b5ab8886a5c9581bc0d3
h3dePL <adamcygan1985@gmail.com>
2025-12-12 12:21:53 +0100

    zmiany w index od lint 2

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document.png",
-        "test": "../bitmaps_test/20251212-092611/Entire_document.png",
+        "test": "../bitmaps_test/20251212-121734/Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
