c3038c6aadf2d0427f30dc0fdd0296cf90af004e
h3dePL <adamcygan1985@gmail.com>
2025-12-15 16:09:12 +0100

    zmiana backstop

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,26 +4,30 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document.png",
-        "test": "../bitmaps_test/20251212-121734/Entire_document.png",
-        "selector": "document",
+        "test": "../bitmaps_test/20251215-160753/Entire_document.png",
+        "selector": "",
         "fileName": "Entire_document.png",
         "label": "Entire document",
         "requireSameDimensions": false,
         "misMatchThreshold": 1,
-        "url": "http://localhost:3001/index.html",
+        "url": "http://localhost:8080/index.html",
         "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
         "expect": 0,
         "viewportLabel": "custom-size",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
         "diff": {
-          "isSameDimensions": true,
+          "isSameDimensions": false,
           "dimensionDifference": {
-            "width": 0,
-            "height": 0
+            "width": -32,
+            "height": -130
           },
-          "misMatchPercentage": "0.00"
-        }
+          "rawMisMatchPercentage": 23.87212643678161,
+          "misMatchPercentage": "23.87",
+          "analysisTime": 3
+        },
+        "diffImage": "../bitmaps_test/20251215-160753/failed_diff_Entire_document.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ]
 });
