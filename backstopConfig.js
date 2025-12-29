4f79b8bec321448738e34bd435b2594d787fb678
Anna Skobara <annaskobara14@gmail.com>
2025-12-29 15:34:28 +0200

    Fix backstop config

--- backstopConfig.js
+++ backstopConfig.js
@@ -4,6 +4,8 @@ const backstop = require('@mate-academy/backstop-config');
 const { basicScenario } = backstop;
 
 const config = {
+  ...backstop,
+
   scenarios: [
     {
       ...basicScenario,
@@ -11,12 +13,6 @@ const config = {
       referenceUrl: basicScenario.referenceUrl + '/hello-world/',
     },
   ],
-
-  engine: 'puppeteer',
-  engineOptions: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  },
 };
 
 module.exports = config;
-
