2934966c98593763d210d7a79ec0235d2cef4ea5
Anna Skobara <annaskobara14@gmail.com>
2025-12-29 15:39:28 +0200

    Fix backstop config

--- backstopConfig.js
+++ backstopConfig.js
@@ -1,10 +1,19 @@
 'use strict';
 
+// https://github.com/garris/BackstopJS#advanced-scenarios
+
 const backstop = require('@mate-academy/backstop-config');
+
 const { basicScenario } = backstop;
 
 const config = {
   ...backstop,
+   fileNameTemplate: '{scenarioLabel}',
+  viewports: [{
+    name: 'custom-size',
+    width: 200,
+    height: 50,
+  }],
 
   scenarios: [
     {
